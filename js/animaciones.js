d5725b6f5a23f90aeba8c7af7d3785b288b15356
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-13 12:16:27 +0200

    Update animaciones.js

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,1 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.from("h1", { duration: 1, y: -50, opacity: 0 });
+  gsap.from("nav ul li", {
+    duration: 0.5,
+    x: -20,
+    opacity: 0,
+    stagger: 0.2
+  });
+});
