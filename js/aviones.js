8b350a248089849ed8d602d36fa1618a569ed168
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-13 12:13:25 +0200

    Update aviones.js

--- js/aviones.js
+++ js/aviones.js
@@ -1,1 +1,16 @@
-
+function mostrarAviones() {
+  const aviones = [
+    "Eurofighter Typhoon",
+    "F/A-18 Hornet",
+    "C-295",
+    "A400M Atlas",
+    "P-3 Orion"
+  ];
+  const lista = document.getElementById("lista-aviones");
+  lista.innerHTML = "";
+  aviones.forEach(avion => {
+    const item = document.createElement("li");
+    item.textContent = avion;
+    lista.appendChild(item);
+  });
+}
