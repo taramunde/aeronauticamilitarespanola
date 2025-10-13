84502853c180d86d79dd81ece4e268e7d86361eb
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-13 12:11:23 +0200

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,12 @@
-
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("menu").innerHTML = 
+    <ul>
+      <li><a href="index.html">Inicio</a></li>
+      <li><a href="historia.html">Historia</a></li>
+      <li><a href="aviones.html">Aviones</a></li>
+      <li><a href="galeria.html">Galería</a></li>
+      <li><a href="mapa.html">Mapa</a></li>
+      <li><a href="linea-tiempo.html">Línea de tiempo</a></li>
+    </ul>
+  ;
+});
