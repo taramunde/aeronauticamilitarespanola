06b6ffb355cfcef580537124e240a7b134f1ab9f
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-13 12:35:26 +0200

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,1 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("aeronautica-cache").then(cache => {
+      return cache.addAll([
+        "/index.html",
+        "/css/base.css",
+        "/js/nav.js",
+        "/assets/img/eurofighter.jpg"
+      ]);
+    })
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
