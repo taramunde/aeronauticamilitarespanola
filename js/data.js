c332e7fe8c84dcf1eab7b2a383d909635b76a885
taramunde <63210114+taramunde@users.noreply.github.com>
2025-10-13 17:26:28 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,12 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("menu").innerHTML = 
-    <ul>
-      <li><a href="index.html">Inicio</a></li>
-      <li><a href="historia.html">Historia</a></li>
-      <li><a href="aviones.html">Aviones</a></li>
-      <li><a href="galeria.html">Galería</a></li>
-      <li><a href="mapa.html">Mapa</a></li>
-      <li><a href="linea-tiempo.html">Línea de tiempo</a></li>
-    </ul>
-  ;
-});
+const aircraftData = {
+    eurofighter: {
+        title: "Eurofighter Typhoon",
+        description: "El Eurofighter Typhoon es un caza polivalente de cuarta generación, diseñado para misiones de superioridad aérea, ataque a tierra y reconocimiento. Entró en servicio en el Ejército del Aire español en 2003."
+    },
+    c101: {
+        title: "CASA C-101 Aviojet",
+        description: "El CASA C-101 Aviojet es un avión de entrenamiento avanzado y ataque ligero, utilizado por la Patrulla Águila para demostraciones aéreas. Fue diseñado por CASA y entró en servicio en 1980."
+    }
+};
